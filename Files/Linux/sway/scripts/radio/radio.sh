33fc7253ade8359a057070ecd97129957113e6ec
Natalie Niemeir <nniemeir@protonmail.com>
2023-12-10 18:51:57 -0600

    Refined radio script

---
+++ Files/Linux/sway/scripts/radio/radio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo "Select a Station: "
+stations=("/home/natalie/.dotfiles/Files/Linux/sway/scripts/radio/stations.txt")
+declare -i count=1
+while IFS= read -r line; do
+  printf "%d: %s\n" "$count" "$(echo "$line" | sed -n "${chosen} s/;.*//p")"
+  let "count++"
+done < "$stations"
+read -r -s chosen
+stationName=$(sed -n "${chosen} s/;.*//p" $stations)
+link=$(sed -n "${chosen} s/^[^;]*;//p" $stations)
+if [ "$link" != "" ]; then 
+   printf "\nNow playing: $stationName\nPause Playback: P\nAbort Playback: Q\n"
+   mpv $link --no-video --really-quiet
+else
+   exit
+fi
