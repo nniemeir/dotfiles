a871301e382794c8ccb19d671a0ccead4c480065
Natalie Niemeir <nniemeir@protonmail.com>
2023-12-11 21:11:44 -0600

    Work continues

--- Files/Linux/sway/scripts/updateCheck.sh
+++ Files/Linux/sway/scripts/updateCheck.sh
@@ -1,6 +1,10 @@
-lastUpdate=$(dnf history | grep upgrade | tail -1 | awk '{ print $6 }') 
+lastUpdate=$(dnf history --reverse | grep "upgrade -y" | tail -1 | awk '{ print $6 }') 
 lastUpdateEpoch=$(date --date="$lastUpdate 00:00:00" +"%s")
 currentTime=$(date +%s)
 timeDiff=$(echo "$(($currentTime-$lastUpdateEpoch))")
 daysSince=$(printf "%f\n" $((timeDiff/86400)))
+if [ $daysSince != 0]; then
 notify-send "Last System Update: ${daysSince%.*} days ago"
+else
+notify-send "Last System Update: Today"
+fi
