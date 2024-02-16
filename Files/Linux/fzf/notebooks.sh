4f9f35f5d9090fdd518367bc097f5723522ba785
Niemeir <91906070+nniemeir@users.noreply.github.com>
2024-02-16 09:22:48 -0600

    Add numbering to file preview

--- Files/Linux/fzf/notebooks.sh
+++ Files/Linux/fzf/notebooks.sh
@@ -4,7 +4,7 @@ while [ $finished == "0" ]; do
 clear
 notePath="$HOME/MEGA/Academic/Notes/"
 noteFiles=$(ls "$notePath/"*.{md,tex} 2>/dev/null)
-selection=$(echo "$noteFiles" | xargs -I {} basename "{}" | fzf --preview="cat $HOME/MEGA/Academic/Notes/{}" --preview-window=right:70%:wrap --reverse)
+selection=$(echo "$noteFiles" | xargs -I {} basename "{}" | fzf --preview="cat -n $HOME/MEGA/Academic/Notes/{}" --preview-window=right:70%:wrap --reverse)
 if [ -z "$selection" ]; then
 	finished="1"
 else
