368d6c19be16dd300d237187a9e8b2fa1bac917a
Jacob Niemeir <nniemeir@protonmail.com>
2025-03-19 14:55:12 -0500

    Fixed inconsistency in toConfigDir

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # Author: Jacob Niemeir <nniemeir@protonmail.com>
 # This script will automatically create symlinks for the included configuration files
-#!/bin/bash
+
 dotsPath="$HOME/.dotfiles/Files/Linux"
-toConfigDir=('dunst' "fzf" 'hypr' 'kitty' 'mpv' 'nvim' 'ranger' 'rofi' 'sway' 'waybar' 'zathura')
+toConfigDir=('dunst' 'fzf' 'hypr' 'kitty' 'mpv' 'nvim' 'ranger' 'rofi' 'sway' 'waybar' 'zathura')
 toHomeDir=('.zshrc')
 
 for item in "${toConfigDir[@]}"; do
